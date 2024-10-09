7367ef9d60bb22279463d2bd3f24cc4289389066
EomUhyeon <righthyeon00@gmail.com>
2024-10-09 10:27:05 +0900

    start

---
+++ server/src/main/java/com/reservoirmap/server/ReservoirMapServerApplication.java
@@ -0,0 +1,13 @@
+package com.reservoirmap.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservoirMapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservoirMapServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/reservoirmap/server/ReservoirMapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservoirmap.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservoirMapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
